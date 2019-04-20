76c6fc3e397f9388902ef24e7b16495d5815b08c
Jaakob Lidauer <jaakob.lidauer@gmail.com>
2019-04-20 17:44:05 +0200

    added to map_node clustering, but it is not working

--- src/map_node.cpp
+++ src/map_node.cpp
@@ -8,18 +8,25 @@
 
 #include <pcl/filters/conditional_removal.h>
 #include <pcl/filters/extract_indices.h>
+#include <pcl/filters/radius_outlier_removal.h>
+
+// Clustering
+#include <pcl/ModelCoefficients.h>
+#include <pcl/common/common.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/kdtree/kdtree.h>
+#include <pcl/segmentation/extract_clusters.h>
 
 typedef pcl::PointXYZRGBA PointType;
 typedef pcl::PointCloud<PointType> PointCloud;
 typedef pcl::PointCloud<PointType>::Ptr PointCloudPtr;
 
-typedef pcl::PointXYZRGBA PointTypeRGB;
+typedef pcl::PointXYZRGB PointTypeRGB;
 typedef pcl::PointCloud<PointTypeRGB> PointCloudRGB;
 typedef pcl::PointCloud<PointTypeRGB>::Ptr PointCloudPtrRGB;
 
 class PlayNode {
-public:
-
+   public:
     PlayNode(int argc, char **argv) {
         ros::init(argc, argv, "map_node");
         n = std::make_unique<ros::NodeHandle>();
@@ -33,22 +40,22 @@ public:
 
         ROS_INFO("Waiting for pointcloud_node/detected_objects");
         ros::topic::waitForMessage<PointCloud>(
-                "pointcloud_node/detected_objects");
+            "pointcloud_node/detected_objects");
 
         detected_objects =
-                n->subscribe("pointcloud_node/detected_objects", 1,
-                &PlayNode::detected_objects_callback, this);
+            n->subscribe("pointcloud_node/detected_objects", 1,
+                         &PlayNode::detected_objects_callback, this);
     }
 
-    void detected_objects_callback(const PointCloudRGB::ConstPtr &msg) {
+    void detected_objects_callback(const PointCloud::ConstPtr &msg) {
         // ROS_INFO("Got new detected objects");
 
         detected_objects_msg = *msg;
         got_detected_objects = true;
     }
 
-    void pub_pointcloud(PointCloudRGB &cloud) {
-        PointCloudPtrRGB msg(new PointCloudRGB);
+    void pub_pointcloud(PointCloud &cloud) {
+        PointCloudPtr msg(new PointCloud);
         msg->header.frame_id = "robot1/odom";
 
         msg->height = cloud.height;
@@ -87,21 +94,183 @@ public:
         }
     }
 
-    /*
-        PointCloudPtrRGB to_rgb_cloud(PointCloudPtr &cloud_rgba) {
-            PointCloudPtrRGB cloud_rgb = PointCloudPtrRGB(new PointCloudRGB());
-            copyPointCloud(*cloud_rgba, *cloud_rgb); // convert rgba -> rgb
-            return cloud_rgb;
+    void radius_outlier_removal(PointCloudPtr &cloud, double radius = 0.05,
+                                int min_neighbors = 5) {
+        pcl::RadiusOutlierRemoval<PointType> outrem;
+        // build the filter
+        outrem.setInputCloud(cloud);
+        outrem.setRadiusSearch(radius);
+        outrem.setMinNeighborsInRadius(min_neighbors);
+        // apply filter
+        outrem.filter(*cloud);
+    }
+
+    PointType is_buck_or_pole(PointCloudPtr &cloud) {
+        // This method takes as input a pointcloud of a suspected pole or puck.
+        // Returns a point containing information (x,y,z,r,g,b) of the object if
+        // it fulfills the requirements.
+
+        int color_threshold = 3;  // min number of points in main color
+        PointType result_point = PointType();
+        result_point.x = 0;
+        result_point.y = 0;
+        result_point.z = 0.2;
+
+        PointType min_point;
+        PointType max_point;
+        pcl::getMinMax3D(*cloud, min_point, max_point);
+
+        // Calculate metrics of the pointcloud
+        double average_x = 0.5 * (max_point.x + min_point.x);
+        double average_y = 0.5 * (max_point.y + min_point.y);
+        double diagonal_xy = sqrt(pow(max_point.y - min_point.y, 2) +
+                                  pow(max_point.x - min_point.x, 2));
+
+        // Check if size of the pointcloud is within the limits
+        if (diagonal_xy >= 0.18) {
+            std::cout << "too big, diagonal: " << diagonal_xy << std::endl;
+            result_point.rgba = 0;
+            result_point.x = 0;
+            result_point.y = 0;
+
+            // size does not match -> return a black point in origo
+            return result_point;
         }
-     */
+
+        // Calculate values for colors
+        uint32_t yellow = 0xffff00;
+        uint32_t blue = 0x0000ff;
+        uint32_t green = 0x00ff00;
+
+        // Calculate color frequencies
+        int blue_points = 0;
+        int green_points = 0;
+        int yellow_points = 0;
+        for (auto pt = cloud->begin(); pt < cloud->end(); pt++) {
+            
+            uint32_t pt_rgb = *reinterpret_cast<int*>(&pt->rgba) & 0xffffff; 
+            //std::cout << std::hex << pt_rgb << std::endl;
+            if (pt_rgb == yellow) {
+                yellow_points++;
+            } else if (pt_rgb == green) {
+                green_points++;
+            } else if (pt_rgb == blue) {
+                blue_points++;
+            }
+        }
+
+        std::cout <<"b"<< blue_points << " g" << green_points << " y"
+                  << yellow_points << std::endl;
+
+        // Determine to which catecory the object belongs to
+        float choosen_color = 0;
+        if (blue_points > green_points && blue_points > yellow_points &&
+            blue_points > color_threshold) {
+            // Blue
+            choosen_color = blue;
+
+        } else if (green_points > blue_points && green_points > yellow_points &&
+                   green_points > color_threshold) {
+            // Green
+            choosen_color = green;
+
+        } else if (yellow_points > green_points &&
+                   yellow_points > blue_points &&
+                   yellow_points > color_threshold) {
+            // Yellow
+            choosen_color = yellow;
+        } else {
+            // Put unknown objects to origo
+            average_x = 0;
+            average_y = 0;
+            std::cout << "unkown cloud" << std::endl;
+        }
+
+        // Return point, color represents point type: green -> buck, blue or
+        // yellow -> pole.
+        result_point.rgba = choosen_color;
+        result_point.x = average_x;
+        result_point.y = average_y;
+
+        return result_point;
+    }
+
+    PointCloudPtr combine_measurements(PointCloudPtr &cloud) {
+        // This algorithm uses Euclidean Cluster Extraction to segment the cloud
+        // into regions. After segmentation objects are filtered by size (min
+        // and max xy coordinates). Finally, the objects are detectect by the
+        // highlighted colors.
+
+        PointCloudPtr result(new PointCloud);
+
+        // Creating the KdTree object for the search method of the extraction
+        pcl::search::KdTree<PointType>::Ptr tree(
+            new pcl::search::KdTree<PointType>);
+        tree->setInputCloud(cloud);
+
+        std::vector<pcl::PointIndices> cluster_indices;
+        pcl::EuclideanClusterExtraction<PointType> ec;
+        ec.setClusterTolerance(0.05);  // 5cm
+        ec.setMinClusterSize(5);
+        ec.setMaxClusterSize(500);
+        ec.setSearchMethod(tree);
+        ec.setInputCloud(cloud);
+        ec.extract(cluster_indices);
+
+        int j = 1;
+        for (std::vector<pcl::PointIndices>::const_iterator it =
+                 cluster_indices.begin();
+             it != cluster_indices.end(); ++it) {
+            pcl::PointCloud<PointType>::Ptr cloud_cluster(
+                new pcl::PointCloud<PointType>);
+
+            for (std::vector<int>::const_iterator pit = it->indices.begin();
+                 pit != it->indices.end(); ++pit) {
+                cloud_cluster->points.push_back(cloud->points[*pit]);
+            }
+
+            cloud_cluster->width = cloud_cluster->points.size();
+            cloud_cluster->height = 1;
+            cloud_cluster->is_dense = true;
+
+            // std::cout << "PointCloud representing the Cluster: "
+            //          << cloud_cluster->points.size() << " data points."
+            //          << std::endl;
+            PointType point = is_buck_or_pole(cloud_cluster);
+            std::cout << point.x << " " << point.y << " j" << j << std::endl;
+            if (point.rgba != 0) {
+                // Add only succesfull detections
+                result->points.push_back(point);
+            }
+
+            j++;
+        }
+
+        // Set header information
+        result->is_dense = false;
+        result->width = 1;
+        result->height = result->points.size();
+
+        return result;
+    }
+
+    void to_environment(PointCloudPtr &cloud) {
+        // This function takes the sum of detectect objets and based on that
+        // finds an estimate of the real buck and pole locations in the
+        // environment.
+
+        // Filter outliers
+        radius_outlier_removal(cloud, 0.03, 5);
+
+        // Cluster cloud
+        cloud = combine_measurements(cloud);
+    }
 
     void process_messages() {
-        // Add new detections to map
-        copyPointCloud(detected_objects_msg,
-                *temp_cloud); // convert rgb -> rgba
-        *map_cloud += *temp_cloud;
+        // Add new detections to map_cloud
+        *map_cloud += detected_objects_msg;
 
-
+        // Update current map_cloud, e.g remove old stuff
         if (alpha_counter >= INCREASE_ALPHA_AFTER_N_MESSAGES) {
             // Increase alpha by one
             for (auto pt = map_cloud->begin(); pt != map_cloud->end(); ++pt) {
@@ -114,7 +283,6 @@ public:
             // Remove points with value 255
             alpha_filter(map_cloud, 255);
             alpha_counter = 0;
-
         } else {
             alpha_counter++;
         }
@@ -123,18 +291,18 @@ public:
         // TODO
 
         // Publish map
-        *temp_cloud = *map_cloud; // copy
+        *temp_cloud = *map_cloud;  // copy
         set_alpha(temp_cloud, 255);
-        pub_pointcloud(*temp_cloud);
+        //to_environment(temp_cloud);
 
-        got_detected_objects = false; // reset
+        pub_pointcloud(*temp_cloud);  // sum of all detections
+
+        got_detected_objects = false;  // reset
     }
 
-    bool got_messages() const {
-        return got_detected_objects;
-    }
+    bool got_messages() const { return got_detected_objects; }
 
-private:
+   private:
     bool got_detected_objects;
     const int INCREASE_ALPHA_AFTER_N_MESSAGES = 2;
     int alpha_counter = 0;
@@ -146,7 +314,7 @@ private:
     // tf2_ros::Buffer *tfBuffer;
     // tf2_ros::TransformListener *tf_listener;
 
-    PointCloudRGB detected_objects_msg;
+    PointCloud detected_objects_msg;
     PointCloudPtr map_cloud;
     PointCloudPtr temp_cloud;
 };
--- src/pointcloud_processor.hpp
+++ src/pointcloud_processor.hpp
@@ -21,29 +21,12 @@
 #include <pcl/kdtree/kdtree.h>
 #include <pcl/segmentation/extract_clusters.h>
 
-// Recognition
-#include <pcl/common/transforms.h>
-#include <pcl/correspondence.h>
-#include <pcl/features/board.h>
-#include <pcl/features/normal_3d_omp.h>
-#include <pcl/features/shot_omp.h>
-#include <pcl/kdtree/kdtree_flann.h>
-#include <pcl/keypoints/uniform_sampling.h>
-#include <pcl/recognition/cg/geometric_consistency.h>
-#include <pcl/recognition/cg/hough_3d.h>
-#include <pcl/kdtree/impl/kdtree_flann.hpp>
-
 #include <pcl/io/pcd_io.h>
 #include <chrono>
-
 
 typedef pcl::PointXYZRGB PointType;
 typedef pcl::PointCloud<PointType> PointCloud;
 typedef pcl::PointCloud<PointType>::Ptr PointCloudPtr;
-
-typedef pcl::Normal NormalType;
-typedef pcl::ReferenceFrame RFType;
-typedef pcl::SHOT352 DescriptorType;
 
 class PointcloudProcessor {
    public:
@@ -61,10 +44,10 @@ class PointcloudProcessor {
         pointcloud_not_floor_blue = PointCloudPtr(new PointCloud);
         pointcloud_not_floor_yellow = PointCloudPtr(new PointCloud);
 
-        pointcloud_puck_model = PointCloudPtr(new PointCloud);
-        pointcloud_pole_model = PointCloudPtr(new PointCloud);
-        generate_puck_pointcloud(pointcloud_puck_model);
-        generate_pole_pointcloud(pointcloud_pole_model);
+        //pointcloud_puck_model = PointCloudPtr(new PointCloud);
+        //pointcloud_pole_model = PointCloudPtr(new PointCloud);
+        //generate_puck_pointcloud(pointcloud_puck_model);
+        //generate_pole_pointcloud(pointcloud_pole_model);
 
         pass_through_filter = pcl::PassThrough<pcl::PointXYZRGB>();
         voxel_grid_filter = pcl::VoxelGrid<pcl::PointXYZRGB>();
@@ -309,6 +292,7 @@ class PointcloudProcessor {
         outrem.filter(*cloud_out);
     }
 
+    /*
     void generate_puck_pointcloud(PointCloudPtr& cloud) {
         double radius = 0.05;  // m
         double radius_second_part = 0.0325;
@@ -385,208 +369,7 @@ class PointcloudProcessor {
         cloud->width = 1;
         cloud->height = cloud->points.size();
     }
-
-    void object_recognition(PointCloudPtr& scene, PointCloudPtr& model) {
-        // Algorithm params
-        bool show_keypoints_(false);
-        bool show_correspondences_(false);
-        bool use_cloud_resolution_(false);
-        bool use_hough_(false);
-        float model_ss_(0.01f);
-        float scene_ss_(0.03f);
-        float rf_rad_(0.015f);
-        float descr_rad_(0.02f);
-        float cg_size_(10 * 0.01f);
-        float cg_thresh_(10 * 5.0f);
-
-        pcl::PointCloud<PointType>::Ptr model_keypoints(
-            new pcl::PointCloud<PointType>());
-        pcl::PointCloud<PointType>::Ptr scene_keypoints(
-            new pcl::PointCloud<PointType>());
-        pcl::PointCloud<NormalType>::Ptr model_normals(
-            new pcl::PointCloud<NormalType>());
-        pcl::PointCloud<NormalType>::Ptr scene_normals(
-            new pcl::PointCloud<NormalType>());
-        pcl::PointCloud<DescriptorType>::Ptr model_descriptors(
-            new pcl::PointCloud<DescriptorType>());
-        pcl::PointCloud<DescriptorType>::Ptr scene_descriptors(
-            new pcl::PointCloud<DescriptorType>());
-
-        //
-        //  Compute Normals
-        //
-        pcl::NormalEstimationOMP<PointType, NormalType> norm_est;
-        norm_est.setKSearch(10);
-        norm_est.setInputCloud(model);
-        norm_est.compute(*model_normals);
-
-        norm_est.setInputCloud(scene);
-        norm_est.compute(*scene_normals);
-
-        //
-        //  Downsample Clouds to Extract keypoints
-        //
-        pcl::UniformSampling<PointType> uniform_sampling;
-        uniform_sampling.setInputCloud(model);
-        uniform_sampling.setRadiusSearch(model_ss_);
-        // uniform_sampling.filter(*model_keypoints);
-        pcl::PointCloud<int> keypointIndices1;
-        uniform_sampling.compute(keypointIndices1);
-        pcl::copyPointCloud(*model, keypointIndices1.points, *model_keypoints);
-
-        std::cout << "Model total points: " << model->size()
-                  << "; Selected Keypoints: " << model_keypoints->size()
-                  << std::endl;
-
-        uniform_sampling.setInputCloud(scene);
-        uniform_sampling.setRadiusSearch(scene_ss_);
-        // uniform_sampling.filter(*scene_keypoints);
-        pcl::PointCloud<int> keypointIndices2;
-        uniform_sampling.compute(keypointIndices2);
-        pcl::copyPointCloud(*scene, keypointIndices2.points, *scene_keypoints);
-
-        std::cout << "Scene total points: " << scene->size()
-                  << "; Selected Keypoints: " << scene_keypoints->size()
-                  << std::endl;
-
-        //
-        //  Compute Descriptor for keypoints
-        //
-        pcl::SHOTEstimationOMP<PointType, NormalType, DescriptorType> descr_est;
-        descr_est.setRadiusSearch(descr_rad_);
-
-        descr_est.setInputCloud(model_keypoints);
-        descr_est.setInputNormals(model_normals);
-        descr_est.setSearchSurface(model);
-        descr_est.compute(*model_descriptors);
-
-        descr_est.setInputCloud(scene_keypoints);
-        descr_est.setInputNormals(scene_normals);
-        descr_est.setSearchSurface(scene);
-        descr_est.compute(*scene_descriptors);
-
-        //
-        //  Find Model-Scene Correspondences with KdTree
-        //
-        pcl::CorrespondencesPtr model_scene_corrs(new pcl::Correspondences());
-
-        pcl::KdTreeFLANN<DescriptorType> match_search;
-        match_search.setInputCloud(model_descriptors);
-
-        //  For each scene keypoint descriptor, find nearest neighbor into the
-        //  model keypoints descriptor cloud and add it to the correspondences
-        //  vector.
-        for (size_t i = 0; i < scene_descriptors->size(); ++i) {
-            std::vector<int> neigh_indices(1);
-            std::vector<float> neigh_sqr_dists(1);
-            if (!std::isfinite(
-                    scene_descriptors->at(i).descriptor[0]))  // skipping NaNs
-            {
-                continue;
-            }
-            int found_neighs = match_search.nearestKSearch(
-                scene_descriptors->at(i), 1, neigh_indices, neigh_sqr_dists);
-            if (found_neighs == 1 &&
-                neigh_sqr_dists[0] <
-                    0.25f)  //  add match only if the squared descriptor
-                            //  distance is less than 0.25 (SHOT descriptor
-                            //  distances are between 0 and 1 by design)
-            {
-                pcl::Correspondence corr(neigh_indices[0], static_cast<int>(i),
-                                         neigh_sqr_dists[0]);
-                model_scene_corrs->push_back(corr);
-            }
-        }
-        std::cout << "Correspondences found: " << model_scene_corrs->size()
-                  << std::endl;
-
-        //
-        //  Actual Clustering
-        //
-        std::vector<Eigen::Matrix4f, Eigen::aligned_allocator<Eigen::Matrix4f> >
-            rototranslations;
-        std::vector<pcl::Correspondences> clustered_corrs;
-
-        //  Using Hough3D
-        if (use_hough_) {
-            //
-            //  Compute (Keypoints) Reference Frames only for Hough
-            //
-            pcl::PointCloud<RFType>::Ptr model_rf(
-                new pcl::PointCloud<RFType>());
-            pcl::PointCloud<RFType>::Ptr scene_rf(
-                new pcl::PointCloud<RFType>());
-
-            pcl::BOARDLocalReferenceFrameEstimation<PointType, NormalType,
-                                                    RFType>
-                rf_est;
-            rf_est.setFindHoles(true);
-            rf_est.setRadiusSearch(rf_rad_);
-
-            rf_est.setInputCloud(model_keypoints);
-            rf_est.setInputNormals(model_normals);
-            rf_est.setSearchSurface(model);
-            rf_est.compute(*model_rf);
-
-            rf_est.setInputCloud(scene_keypoints);
-            rf_est.setInputNormals(scene_normals);
-            rf_est.setSearchSurface(scene);
-            rf_est.compute(*scene_rf);
-
-            //  Clustering
-            pcl::Hough3DGrouping<PointType, PointType, RFType, RFType>
-                clusterer;
-            clusterer.setHoughBinSize(cg_size_);
-            clusterer.setHoughThreshold(cg_thresh_);
-            clusterer.setUseInterpolation(true);
-            clusterer.setUseDistanceWeight(false);
-
-            clusterer.setInputCloud(model_keypoints);
-            clusterer.setInputRf(model_rf);
-            clusterer.setSceneCloud(scene_keypoints);
-            clusterer.setSceneRf(scene_rf);
-            clusterer.setModelSceneCorrespondences(model_scene_corrs);
-
-            // clusterer.cluster (clustered_corrs);
-            clusterer.recognize(rototranslations, clustered_corrs);
-        } else {
-            // Using GeometricConsistency
-            pcl::GeometricConsistencyGrouping<PointType, PointType>
-                gc_clusterer;
-            gc_clusterer.setGCSize(cg_size_);
-            gc_clusterer.setGCThreshold(cg_thresh_);
-
-            gc_clusterer.setInputCloud(model_keypoints);
-            gc_clusterer.setSceneCloud(scene_keypoints);
-            gc_clusterer.setModelSceneCorrespondences(model_scene_corrs);
-
-            // gc_clusterer.cluster (clustered_corrs);
-            gc_clusterer.recognize(rototranslations, clustered_corrs);
-        }
-
-        std::cout << "Model instances found: " << rototranslations.size()
-                  << std::endl;
-        for (size_t i = 0; i < rototranslations.size(); ++i) {
-            std::cout << "\n    Instance " << i + 1 << ":" << std::endl;
-            std::cout << "        Correspondences belonging to this instance: "
-                      << clustered_corrs[i].size() << std::endl;
-
-            // Print the rotation matrix and translation vector
-            Eigen::Matrix3f rotation = rototranslations[i].block<3, 3>(0, 0);
-            Eigen::Vector3f translation = rototranslations[i].block<3, 1>(0, 3);
-
-            printf("\n");
-            printf("            | %6.3f %6.3f %6.3f | \n", rotation(0, 0),
-                   rotation(0, 1), rotation(0, 2));
-            printf("        R = | %6.3f %6.3f %6.3f | \n", rotation(1, 0),
-                   rotation(1, 1), rotation(1, 2));
-            printf("            | %6.3f %6.3f %6.3f | \n", rotation(2, 0),
-                   rotation(2, 1), rotation(2, 2));
-            printf("\n");
-            printf("        t = < %0.3f, %0.3f, %0.3f >\n", translation(0),
-                   translation(1), translation(2));
-        }
-    }
+    */
 
     void save_cloud_to_file(PointCloudPtr& cloud, std::string path_and_name) {
         if (cloud->points.size() > 0) {
@@ -812,12 +595,11 @@ class PointcloudProcessor {
 
     void process_pointcloud(PointCloudPtr& cloud) {
         // Processes a new point cloud and extracts useful information
+
         auto start_time = std::chrono::high_resolution_clock::now();
 
         // Reduce nuber of points in the pointcloud
-        // std::cout << *cloud << std::endl;
         voxel_grid_filter_m(cloud, pointcloud_temp2, 0.015, 5);
-        // std::cout << *pointcloud_temp << std::endl;
 
         // Find interesting colors in the cloud (maps specific color regions to
         // a specific color value for filtering the points later)
@@ -826,8 +608,6 @@ class PointcloudProcessor {
         // Segment cloud into floor and not floor
         planar_segmentation(pointcloud_temp2, pointcloud_floor,
                             pointcloud_not_floor);
-        // std::cout << "not floor points: " << *pointcloud_not_floor <<
-        // std::endl;
 
         // save_cloud_to_file(pointcloud_not_floor,
         //"/home/cnc/Desktop/Hockey/PCL-cluster-segmentation-tests/not-floor2.pcd"
@@ -839,13 +619,14 @@ class PointcloudProcessor {
         // Find pucks and poles from not floor pointcoud
         recognized_objects = get_bucks_and_poles(pointcloud_temp2);
 
+        // Calculate used time
         auto end_time = std::chrono::high_resolution_clock::now();
         auto delta_time = end_time - start_time;
         std::cout << "Took " << delta_time / std::chrono::milliseconds(1)
                   << "ms to run.\n";
 
         return;  //----------------------------------------------------
-
+        // Testing stuff:
 
         // Filter region by color:
         // Blue goals
@@ -861,20 +642,6 @@ class PointcloudProcessor {
             radius_outlier_removal(pointcloud_temp, pointcloud_not_floor_blue);
         }
 
-        pcl::removeNaNFromPointCloud(*pointcloud_not_floor_green,
-                                     *pointcloud_temp, indices);
-        if (pointcloud_not_floor_green->points.size() > 0) {
-            radius_outlier_removal(pointcloud_temp, pointcloud_not_floor_green);
-        }
-
-        pcl::removeNaNFromPointCloud(*pointcloud_not_floor_yellow,
-                                     *pointcloud_temp, indices);
-        if (pointcloud_not_floor_yellow->points.size() > 0) {
-            radius_outlier_removal(pointcloud_temp,
-                                   pointcloud_not_floor_yellow);
-        }
-
-        // TODO ...
 
         edit_z_to(pointcloud_floor, 0);
         edit_z_to(pointcloud_not_floor_green, 0.2);
@@ -917,6 +684,6 @@ class PointcloudProcessor {
 
     PointCloudPtr recognized_objects;
 
-    PointCloudPtr pointcloud_puck_model;
-    PointCloudPtr pointcloud_pole_model;
+    //PointCloudPtr pointcloud_puck_model;
+    //PointCloudPtr pointcloud_pole_model;
 };
